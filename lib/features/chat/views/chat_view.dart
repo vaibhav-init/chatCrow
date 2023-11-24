0f7d7e55d108b3e8b9c6be559604dde9f91d7cb8
Vaibhav <vaibhavlakhera1729@gmail.com>
2023-11-24 16:03:51 +0530

    chat views modified

--- lib/features/chat/views/chat_view.dart
+++ lib/features/chat/views/chat_view.dart
@@ -18,7 +18,7 @@ class MobileChatScreen extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 10),
             child: Icon(
-              Icons.video_call,
+              Icons.videocam,
               size: 27,
             ),
           ),
@@ -38,91 +38,70 @@ class MobileChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/bgImg.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-                child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      prefixIcon: IconButton(
-                        onPressed: () {},
-                        color: Colors.grey,
-                        icon: const Icon(
-                          Icons.emoji_emotions,
-                          size: 26,
-                        ),
-                      ),
-                      hintText: 'Message',
-                      hintStyle:
-                          const TextStyle(fontSize: 10, color: Colors.white),
-                      suffixIcon: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            color: Colors.grey,
-                            icon: const Icon(
-                              Icons.attach_file,
-                              size: 20,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            color: Colors.grey,
-                            icon: const Icon(
-                              Icons.currency_rupee,
-                              size: 20,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            color: Colors.grey,
-                            icon: const Icon(
-                              Icons.camera_alt,
-                              size: 20,
-                            ),
-                          ),
-                        ],
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25),
-                        borderSide: const BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
-                      ),
-                      contentPadding: const EdgeInsets.all(9),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: CircleAvatar(
-                    radius: 22,
-                    child: IconButton(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    prefixIcon: IconButton(
                       onPressed: () {},
                       icon: const Icon(
-                        Icons.mic,
-                        color: Colors.white,
-                        size: 25,
+                        Icons.emoji_emotions,
+                        size: 26,
                       ),
+                    ),
+                    hintText: 'Message',
+                    hintStyle:
+                        const TextStyle(fontSize: 10, color: Colors.white),
+                    suffixIcon: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.attach_file,
+                            size: 20,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            size: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.all(9),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: CircleAvatar(
+                  radius: 22,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mic,
+                      size: 25,
                     ),
                   ),
                 ),
-              ],
-            ))
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
