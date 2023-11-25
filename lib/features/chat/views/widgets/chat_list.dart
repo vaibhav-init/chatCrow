d5151e987d1f21ad56ba742215ccc1b20046e4d2
Vaibhav <vaibhavlakhera1729@gmail.com>
2023-11-25 18:52:06 +0530

    scroll done

--- lib/features/chat/views/widgets/chat_list.dart
+++ lib/features/chat/views/widgets/chat_list.dart
@@ -5,10 +5,11 @@ import 'package:chat_crow/features/chat/views/widgets/sender_message_card.dart';
 import 'package:chat_crow/models/message_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
-class ChatList extends ConsumerWidget {
+class ChatList extends ConsumerStatefulWidget {
   final String receiverId;
 
   const ChatList({
@@ -17,14 +18,35 @@ class ChatList extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ChatListState();
+}
+
+class _ChatListState extends ConsumerState<ChatList> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    scrollController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return StreamBuilder<List<Message>>(
-        stream: ref.read(chatControllerProvider).getChats(receiverId),
+        stream: ref.read(chatControllerProvider).getChats(widget.receiverId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loader();
           }
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            scrollController.animateTo(
+              scrollController.position.maxScrollExtent,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeOut,
+            );
+          });
           return ListView.builder(
+            controller: scrollController,
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
               final messageData = snapshot.data![index];
